9d39d8b1fe15736c10407d6fbfc0a6df481645d1
Robin Bron <robin@finwo.nl>
2017-05-29 17:47:11 +0200

    Deep configs supported

--- config/index.js
+++ config/index.js
@@ -14,7 +14,7 @@ function readdir( dir ) {
         var fullPath = path.join( dir, entry ),
             stat     = fs.statSync( fullPath );
         if ( stat.isDirectory() ) {
-          output.concat(readdir( fullPath ));
+          output = output.concat(readdir( fullPath ));
         } else if ( stat.isFile() ) {
           output.push( fullPath );
         }
@@ -25,14 +25,36 @@ function readdir( dir ) {
   return output;
 }
 
+function setDeep( subject, key, value ) {
+  if ( 'string' === typeof key ) {
+    key = key.split('.');
+  }
+  if ( !Array.isArray(key) ) {
+    return;
+  }
+  var part;
+  while(key.length) {
+    part = key.shift();
+    if ( key.length ) {
+      subject = (subject[part]=subject[part]||{});
+    } else {
+      subject[part] = value;
+    }
+  }
+}
+
 readdir(__dirname)
   .forEach(function( file ) {
     file = file.split('.');
     var ext = file.pop();
+    if ( ext !== 'js' ) {
+      return;
+    }
     file = file.join('.');
     var name = file;
     if ( name.substr( 0, __dirname.length ) == __dirname ) {
       name = name.substr( __dirname.length + 1 );
+      name = name.split(path.sep).join('.');
     }
-    module.exports[name] = require(file);
+    setDeep(module.exports,name,require(file));
   });
