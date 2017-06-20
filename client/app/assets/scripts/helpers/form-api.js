9e4713391c83a0ad2ad93a944563b85466ba6fbe
Robin Bron <robin@finwo.nl>
2017-06-20 18:04:57 +0200

    [WIP] form styling

---
+++ client/app/assets/scripts/helpers/form-api.js
@@ -0,0 +1,5 @@
+define(['api'], function(api) {
+  return function($el) {
+    console.log($el);
+  };
+});
---
+++ client/app/assets/scripts/helpers/modal.js
@@ -0,0 +1,8 @@
+define(function() {
+  return function($el) {
+    $el
+      .on('click', function() { document.location.hash = '#!'; })
+      .find('.modal-content')
+      .on('click', function() { return false; })
+  };
+});
