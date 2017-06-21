8893e08bcfc021f8a54fe6097da407fb280cbe5d
Robin Bron <robin@finwo.nl>
2017-06-21 17:03:05 +0200

    Added user options

--- config/database/collections.js
+++ config/database/collections.js
@@ -1,33 +1,33 @@
 var co = require('co');
 
-/* These are only predefined collections
- * The project is set up to allow run-time collections
+/* These are just collection settings, like hooks
  */
 
 module.exports = require('co')(function*() {
   return {
-    document: {
-    },
-
-    rule: {
-
-    },
-
-    user: {
-      beforeCreate: function( resource, data, cb ) {
+    'user': {
+      beforeCreate: function ( resource, data, cb ) {
         return co(function*() {
           if ( !data.password ) throw "Missing password";
           if ( 'string' != typeof data.password ) throw "Password not a string";
 
-          if ( data.password.substr(0,1) != '#' ) {
+          if ( data.password.substr(0, 1) != '#' ) {
             var UserService = yield service('UserService');
-            data.password = yield UserService.encryptPassword(data.password);
+            data.password   = yield UserService.encryptPassword(data.password);
           }
 
           return data;
         });
+      },
+      relations   : {
+        hasMany: {
+          option: {
+            localField: 'options',
+            localKey  : 'username',
+            foreignKey: 'username'
+          }
+        }
       }
     }
-
   };
 });
