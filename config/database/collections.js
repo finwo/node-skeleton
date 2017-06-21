d4c09b72a51fdc7a7064efac05390255ef2b8704
Robin Bron <robin@finwo.nl>
2017-06-21 16:04:59 +0200

    Better bootstrap data

--- config/database/collections.js
+++ config/database/collections.js
@@ -5,51 +5,13 @@
 module.exports = require('co')(function*() {
   return {
 
-    Collection: {
-      schema: {
-        name: {
-          type: String,
-          required: true
-        }
-      }
+    collection: {
     },
 
-    Rule: {
-      schema: {
-        collection: {
-          type: String,
-          required: true
-        },
-        editable: {
-          type: Boolean,
-          required: true
-        },
-        key: {
-          type: String,
-          required: true
-        },
-        type: {
-          type: String,
-          required: true
-        },
-        value: {
-          type: String,
-          required: false
-        }
-      }
+    rule: {
     },
 
-    User: {
-      schema: {
-        username: {
-          type: String,
-          required: true
-        },
-        password: {
-          type: String,
-          required: true
-        }
-      }
+    user: {
     }
 
   };
