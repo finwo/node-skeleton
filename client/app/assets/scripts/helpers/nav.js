ceefe022a0ab8082ebed0bf9df7ac60e251c2882
Robin Bron <robin@finwo.nl>
2017-06-26 14:01:24 +0200

    Added dropdown support to nav

--- client/app/assets/scripts/helpers/nav.js
+++ client/app/assets/scripts/helpers/nav.js
@@ -1,13 +1,22 @@
 define(['jquery'], function($) {
   return function($el) {
+
+    // Make it responsive
     var $container = $el.find('.container'),
         $title     = $el.find('.title'),
-        $button    = $('<a href="#" class="icon">&#9776;</a>');
-    function toggle() {
-      $el.toggleClass('open');
+        $button    = $('<a href="#!" class="icon">&#9776;</a>');
+    function toggle(el) {
+      el = el || $el;
+      el.toggleClass('open');
     }
     $button.on('click', toggle);
     $title.on('click', toggle);
     $container.append($button);
+
+    // Handle dropdowns
+    $el.find('a:not(:last-child)').each(function() {
+      var $this = $(this);
+      $this.on('click', toggle.bind(null,$this));
+    });
   };
 });
