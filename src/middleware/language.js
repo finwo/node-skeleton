c23b95066ac294af8b8079d7ddefb27d297f25f5
Robin Bron <robin@finwo.nl>
2017-06-27 13:54:42 +0200

    Errors not switching language anymore

--- src/middleware/language.js
+++ src/middleware/language.js
@@ -1,6 +1,9 @@
 module.exports = {
   "any *": function*( req, res ) {
     var requested = (req.headers[ 'accept-language' ] || '').split(',');
+    req.url.replace(/^\/([a-z]{2})\//, function(full, lang) {
+      requested.unshift(lang);
+    });
     requested.push(config.language.default);
     req.language = requested
       .map(function ( language ) {
